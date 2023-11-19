dc3aa879cef233054e990b704f21dfe804dfc889
Mukhammad-develop <115652500+Mukhammad-develop@users.noreply.github.com>
2023-11-19 11:54:00 +0500

    migrations added

--- polling_system/views.py
+++ polling_system/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import *
 
 
-# Create your views here.
 def index(request):
-    return render(request , 'main.html')
+    # Fetch data for the index page, such as political parties and polls
+    political_parties = PoliticalParty.objects.all()
+    polls = Poll.objects.all()
+
+    # Pass the data to the template
+    context = {
+        'political_parties': political_parties,
+        'polls': polls,
+    }
+
+    # Render the index page with the provided data
+    return render(request, 'main.html', context)
