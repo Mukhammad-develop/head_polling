b88959c8d96b54a8d3c6b6fa2a70da31ca8c434b
Mukhammad-develop <115652500+Mukhammad-develop@users.noreply.github.com>
2023-11-21 03:03:32 +0500

    list and detail views added

--- polling_system/views.py
+++ polling_system/views.py
@@ -1,17 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 
 def index(request):
-    # Fetch data for the index page, such as political parties and polls
     political_parties = PoliticalParty.objects.all()
     polls = Poll.objects.all()
+    candidates = Candidate.objects.all()
+    votes = Vote.objects.all()
 
-    # Pass the data to the template
     context = {
         'political_parties': political_parties,
         'polls': polls,
+        'candidates': candidates,
+        'votes': votes,
     }
 
-    # Render the index page with the provided data
     return render(request, 'main.html', context)
+
+
+def poll_detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    context = {'poll': poll}
+    return render(request, 'poll_detail.html', context)
+
+
+def poll_list(request):
+    polls = Poll.objects.all()
+
+    context = {'polls': polls}
+    return render(request, 'poll_list.html', context)
+
+
+def candidate_list(request):
+    candidates = Candidate.objects.all()
+    context = {'candidates': candidates}
+    return render(request, 'candidate_list.html', context)
+
+
+def candidate_detail(request, candidate_id):
+    candidate = get_object_or_404(Candidate, pk=candidate_id)
+    context = {'candidate': candidate}
+    return render(request, 'candidate_detail.html', context)
