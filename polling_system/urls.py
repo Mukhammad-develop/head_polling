3bd95e073f8996cf256167102e870c95da4e783b
Mukhammad-develop <115652500+Mukhammad-develop@users.noreply.github.com>
2023-11-21 03:03:11 +0500

    list and detail urls added

--- polling_system/urls.py
+++ polling_system/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('polls/', views.poll_list, name='poll_list'),
+    path('polls/<int:poll_id>/', views.poll_detail, name='poll_detail'),
+    path('candidates/', views.candidate_list, name='candidate_list'),
+    path('candidates/<int:candidate_id>/', views.candidate_detail, name='candidate_detail'),
 
 ]
